bd20d1de7041e3ae24a7d3716b428fe8616b05fd
sasscog <105199203+sasscog@users.noreply.github.com>
2022-06-23 10:12:44 +0100

    Add files via upload

---
+++ display name.js
@@ -0,0 +1,9 @@
+const aboutme = {
+    name: 'Saliu Ali',
+    height: '1.65cm',
+    country: 'Nigeria',
+};
+
+alert(
+    `My name is ${aboutme.name} , i am ${aboutme.height} tall and i  am from ${aboutme.country}.`
+);
